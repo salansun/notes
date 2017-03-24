594582bd58d8e270affc591924023e387eb078ba
Dotos <yy.liu@foxmail.com>
2017-03-24 11:00:55 +0800

    Site updated: 2017-03-24 11:00:50

--- js/src/visibility.js
+++ js/src/visibility.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 23 2017 09:15:38 GMT+0800 (中国标准时间)
+// build time:Fri Mar 24 2017 11:00:32 GMT+0800 (中国标准时间)
 !function(i,t){var e=t.title,n="hidden"in t?"hidden":"webkitHidden"in t?"webKitHidden":"mozHidden"in t?"mozHidden":null,d="visibilityState"in t?"visibilityState":"webkitVisibilityState"in t?"webkitVisibilityState":"mozVisibilityState"in t?"mozVisibilityState":null;if(n&&d){var l=n.replace(/hidden/i,"visibilitychange");t.addEventListener(l,function(i){t.title=t[n]?"(☯_☯)我藏起来了哦~ | "+e:e},!1)}}(window,document);
 //rebuild by neat 
