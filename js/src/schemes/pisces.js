83f26da68b03b5c7c1ea17da2ab1e7add0da19dd
Dotos <yy.liu@foxmail.com>
2017-03-24 11:04:25 +0800

    Site updated: 2017-03-24 11:04:20

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 24 2017 11:00:34 GMT+0800 (中国标准时间)
+// build time:Fri Mar 24 2017 11:04:03 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner"),e=$("#sidebar"),i=function(){return n.height()+CONFIG.sidebar.offset},t=function(n){return e.css({"margin-top":n})},r=window.matchMedia("(min-width: 991px)");t(i()).show(),r.addListener(function(n){n.matches&&t(i())})});
 //rebuild by neat 
